5faef72cc8f8505a9ad5cc17951d53b0b8b2de76
ce424-012-98171053 <amk_amir82@yahoo.com>
2023-06-09 17:04:14 +0330

    AddProjectAPITest

--- todo/tests.py
+++ todo/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
+from todo.models.user import User
+from django.urls import reverse
 from rest_framework.test import APITestCase
 from rest_framework import status
 
@@ -31,3 +32,43 @@ class SignupAPITest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(User.objects.count(), 1)
         self.assertEqual(User.objects.first().username, 'manager')
+
+
+class AddProjectAPITest(APITestCase):
+
+    def setUp(self):
+        self.manager = User.objects.create_user(
+            username='manager',
+            password='password123',
+            email='manager@example.com',
+            role='Project Manager'
+        )
+        self.client.force_authenticate(user=self.manager)
+
+    def test_add_project_as_manager(self):
+        url = reverse('add_project')
+        data = {
+            'name': 'Project 1',
+            'description': 'This is project 1'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['name'], 'Project 1')
+        self.assertEqual(response.data['description'], 'This is project 1')
+
+    def test_add_project_as_developer(self):
+        developer = User.objects.create_user(
+            username='developer',
+            password='password123',
+            email='developer@example.com',
+            role='Developer'
+        )
+        self.client.force_authenticate(user=developer)
+
+        url = reverse('add_project')
+        data = {
+            'name': 'Project 2',
+            'description': 'This is project 2'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
